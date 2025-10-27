924f5060a073b83d11f3782b2a80a003751b47ff
Carlos Guerrero <hi@carlosguerrero.com>
2025-10-27 16:09:52 -0300

    Working Prototype

--- NetSpeedMonitor/MenuBarIconGenerator.swift
+++ NetSpeedMonitor/MenuBarIconGenerator.swift
@@ -1,25 +1,24 @@
 import AppKit
 
 final class MenuBarIconGenerator {
-    
+
     static func generateIcon(
         text: String,
         font: NSFont = .monospacedSystemFont(ofSize: 8, weight: .semibold)
     ) -> NSImage {
         let image = NSImage(size: NSSize(width: 90, height: 22), flipped: false) { rect in
-            
+
             let style = NSMutableParagraphStyle()
             style.alignment = .right
-//            style.maximumLineHeight = 10
-//            style.paragraphSpacing = -5
-            
+            //            style.maximumLineHeight = 10
+            //            style.paragraphSpacing = -5
+
             let attributes: [NSAttributedString.Key: Any] = [
                 .font: font,
-//                .baselineOffset: 0,
-                .paragraphStyle: style
+                //                .baselineOffset: 0,
+                .paragraphStyle: style,
             ]
-            
-            
+
             let textSize = text.size(withAttributes: attributes)
             let textRect = NSRect(
                 x: (rect.width - textSize.width) / 2,
@@ -27,11 +26,11 @@ final class MenuBarIconGenerator {
                 width: textSize.width,
                 height: textSize.height
             )
-            
+
             text.draw(in: textRect, withAttributes: attributes)
             return true
         }
-        
+
         image.isTemplate = true
         return image
     }
